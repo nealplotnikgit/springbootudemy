e31d5b512e9051a13685b6f277219d2a3da0e4b4
Neal Plotnik <nealplotnik@gmail.com>
2020-11-10 20:10:14 -0600

    udemy - add many to one relationship untested

--- src/main/java/com/example/demo/entity/Subject.java
+++ src/main/java/com/example/demo/entity/Subject.java
@@ -1,7 +1,5 @@
 package com.example.demo.entity;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,8 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -26,33 +23,25 @@ import lombok.Setter;
 @Setter
 @Entity
 @NoArgsConstructor //need this because creating the explicit constructor will not create the default one, but this is needed for JPA
-@Table(name = "student")
+@Table(name = "subject")
 //test git
 //default constructor is required - already provided since no other is defined
-public class Student {
+public class Subject {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long  id;
-	@Column(name = "first_name")
-	private String firstName;
-	@Column(name = "last_name")
-	private String lastName;
-	@Column(name = "email")
-	private String email;
+	@Column(name = "subject_name")
+	private String subjectName;
+	@Column(name = "marks_obtained")
+	private Double marks;
 	
-//	@OneToOne(fetch = FetchType.LAZY)
-	@OneToOne(fetch = FetchType.EAGER) //this is default
-	// using the join on the owning side creates a relationship to the child table
-	@JoinColumn(name = "address_id")
-	private Address address;
 	
-	@OneToMany(mappedBy = "student")
-	private List<Subject> subjects;
+	@ManyToOne(fetch = FetchType.EAGER) //this is default
+	// using the join on the owning side creates a relationship to the child table. 
+	//this table is the owner because it contains the foreign key to the student
+	@JoinColumn(name = "student_id")
+	private Student student;
 	
-	public Student(CreateStudentRequest csr) {
-		this.firstName = csr.getFirstName();
-		this.lastName = csr.getLastName();
-		this.email = csr.getEmail();
-	}
+	
 }
